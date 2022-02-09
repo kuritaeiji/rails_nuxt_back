fbf214db6f978a0ea99dae13e71e0ba813040f8c
kuritaeiji <eiji199825@yahoo.co.jp>
2022-02-09 14:21:04 +0900

    修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins(ENV['FRONT_DOMAIN']) || 'rails-nuxt-front.herokuapp.com:3000'
+    origins(ENV['FRONT_DOMAIN'] || 'rails-nuxt-front.herokuapp.com:3000')
 
     resource '*',
       headers: :any,
