31c9d3adb83019f8f77769b835beea339bdf540f
kuritaeiji <eiji199825@yahoo.co.jp>
2022-02-10 11:48:38 +0900

    ユーザーモデルの作成

---
+++ app/models/user.rb
@@ -0,0 +1,31 @@
+class User < ApplicationRecord
+  has_secure_password
+  before_validation(:downcase_email)
+
+  validates(:name, presence: true, length: { maximum: 30, allow_blank: true })
+  VALID_PASSWORD_REGEXP = /\A[\w-]+\z/.freeze
+  validates(
+    :password,
+    length: { minimum: 8, allow_blank: true, with: :invalid_password },
+    format: { with: VALID_PASSWORD_REGEXP, allow_blank: true }
+  )
+  validates(:email, presence: true, email: { allow_blank: true })
+
+  class << self
+    def find_activated(email)
+      find_by(email: email, activated: true)
+    end
+  end
+
+  # 自分以外の同じemailのアクティブなユーザーがいる場合にtrueを返す
+  def email_activated?
+    other_users = User.where.not(id: id)
+    other_users.find_activated(email).present?
+  end
+
+  private
+
+  def downcase_email
+    email&.downcase!
+  end
+end
---
+++ db/seeds/development.rb
@@ -0,0 +1,12 @@
+10.times do |n|
+  name = "user#{n}"
+  email = "#{name}@exmaple.com"
+
+  user = User.find_or_initialize_by(email: email, activated: true)
+
+  next unless user.new_record?
+
+  user.name = name
+  user.password = 'password'
+  user.save!
+end
